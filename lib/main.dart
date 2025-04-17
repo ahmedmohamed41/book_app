702bc06e52c3a94830522c5f7edad0a3445ab398
ahmed khodary <ahmedmohamed2019.com@gmail.com>
2025-04-17 15:15:18 +0200

    create project book_app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text(''));
+  }
+}
